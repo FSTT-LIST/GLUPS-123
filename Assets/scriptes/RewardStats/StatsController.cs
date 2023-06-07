d233808ee85601da21888fa7cb94812918718e99
Ahmed Samady <samadycrm@gmail.com>
2023-06-07 18:46:39 +0100

    v0.32 Final

--- Assets/scriptes/RewardStats/StatsController.cs
+++ Assets/scriptes/RewardStats/StatsController.cs
@@ -74,7 +74,7 @@ public class StatsController : MonoBehaviour
 
         StreamWriter fileWriter = new StreamWriter(new FileStream(filePath, FileMode.Append, FileAccess.Write, FileShare.ReadWrite));
 
-        string csvData = string.Format("{0}; {1}; {2}; {3}.{4}.{5}; {6}; <level ;{7};>; <score ;{8};>; <rank ;{9};>; <iScore ;{10};> ; <nextThreshold ;{11};>; <reward ;{12};>; <positives ;{13};>; <negatives ;{14};>; <seconds ;{15};>",
+        string csvData = string.Format("{0}; <seconds ;{1};>; {2}; {3}; {4}.{5}.{6}; {7}; <level ;{8};>; <score ;{9};>; <rank ;{10};>; <iScore ;{11};> ; <nextThreshold ;{12};>; <reward ;{13};>; <positives ;{14};>; <negatives ;{15};>",
             days, seconds, inputOutput, Application.version, macAddress, id, reversedPlayerNom, Main.controller._currentStrategy.name(), LevelIndex,
             score, rank, iScore, nextThreshold, reward, Main.model.Positives, Main.model.Negatives);
         fileWriter.WriteLine(csvData);
